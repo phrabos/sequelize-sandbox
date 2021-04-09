740fb2854658c5760448ec09124d2acfd3b2334c
patrick hrabos <phrabos@gmail.com>
2021-04-08 23:11:35 -0400

    create movies table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../lib/app');
 const db = require('../lib/utils/database');
-// const Actor = require('../lib/models/Actor');
+const Actor = require('../lib/models/Actor');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -21,6 +21,43 @@ describe('alchemy-app routes', () => {
   name: 'Jim',
   dob: "1943-08-21T04:00:00.000Z",
   pob: 'San Francisco'
+    });
+  }) 
+  it('gets an actor by ID', async()=>{
+
+    await Actor.create({
+      id:1,
+      name: 'Jim',
+      dob: "1943-08-21T04:00:00.000Z",
+      pob: 'San Francisco'
+        })
+
+    const data = await request(app)
+    .get('/api/v1/actors/1')
+
+    expect(data.body).toEqual(
+      {
+        id:1,
+        name: 'Jim',
+        dob: "1943-08-21T04:00:00.000Z",
+        pob: 'San Francisco'
+      }
+    )
   })
-}) 
+  it('create an actor via POST', async()=>{
+    const movie = {
+    title: 'Jurassic Park',
+    year: 1993,
+    actor_id: 1,
+  }
+    const data = await request(app)
+    .post('/api/v1/movies')
+    .send(movie)
+    expect(data.body).toEqual({
+      id:1,
+      title: 'Jurassic Park',
+      year: 1993,
+      actor_id: 1,
+    });
+  }) 
 })
